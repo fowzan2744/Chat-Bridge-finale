fe34d392a20934ffc68189cd63e65c1652603dcf
Fowzan <153728662+fowzan2744@users.noreply.github.com>
2024-12-25 19:30:11 +0530

    razorpayy

---
+++ server/config/razorpay.js
@@ -0,0 +1,26 @@
+const Razorpay = require('razorpay');
+const crypto = require('crypto');
+const dotenv = require("dotenv");
+dotenv.config();
+
+let instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+});
+
+
+const validateSignature = async (razorpayPaymentId, orderCreationId, razorpaySignature) => {
+    const shasum = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
+
+    shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+
+    const digest = shasum.digest("hex");
+
+    console.log(digest, razorpaySignature);
+
+    if (digest === razorpaySignature) return true;
+
+    return false;
+}
+
+module.exports = { instance, validateSignature };
